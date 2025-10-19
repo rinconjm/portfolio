42656531f542f083893d0c39cf032c8031a4d38c
rinconjm <145496094+rinconjm@users.noreply.github.com>
2025-10-19 14:46:41 -0700

    Revised JS

--- global.js
+++ global.js
@@ -96,3 +96,15 @@ form?.addEventListener('submit', function(event) {
     }
     location.href = url;
 });
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
